63a40cc08c3a185dc6b9b3909c36b0c926bb19af
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-20 22:18:00 +0530

    created 'create' request

---
+++ api/Student/create.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import { createStudent } from "./../../src/modules/Student/controller";
+
+const app = express();
+
+
+app.post(
+  "/createStudent",
+  createStudent
+);
+
+export default app;

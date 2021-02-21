6577d4e8df2d7bacdcaee55c21f8152ada53518a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-02-21 12:13:52 +0530

    added delete api

--- api/Student/index.js
+++ api/Student/index.js
@@ -2,6 +2,7 @@
 import create from "./create";
 import view from "./view";
 import update from "./update";
+import remove from "./remove";
 
 
-export default [create, view, update];
+export default [create, view, update, remove];
---
+++ api/Student/remove.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import { deleteStudent } from '../../src/modules/Student/controller';
+
+//Routes
+const app = express();
+
+app.delete('/delete', deleteStudent);
+
+export default app;

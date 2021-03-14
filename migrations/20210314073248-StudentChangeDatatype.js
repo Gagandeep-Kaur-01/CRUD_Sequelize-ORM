6218201e7725415d2c597874d9fa62e5d7951783
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-14 13:08:13 +0530

    changed the datatype of column

--- migrations/20210314073248-StudentChangeDatatype.js
+++ migrations/20210314073248-StudentChangeDatatype.js
@@ -2,20 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return Promise.all([
+      queryInterface.changeColumn(
+        'students', 
+        'quiz', 
+        {
+        type: Sequelize.FLOAT
+        }
+      ),
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return Promise.all([queryInterface.changeColumn('students', 'quiz', {
+      type: Sequelize.INTEGER
+    })]);
   }
 };

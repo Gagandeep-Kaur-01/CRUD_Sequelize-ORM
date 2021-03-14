e5bb6dce0e4f05004992ca3e863b5d25351c15ab
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-03-14 13:00:54 +0530

    run 'sequelize db:migrate' and 'updated the model'

--- src/models/Student.js
+++ src/models/Student.js
@@ -36,7 +36,7 @@ module.exports = (sequelize, DataTypes) => {
 			defaultValue: '',
 		},
 		quiz: {
-			type: DataTypes.STRING,
+			type: DataTypes.INTEGER,
 			allowNull: false,
 			defaultValue: '',
 		},
